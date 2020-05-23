9d1885c0d4188cae38929f74f6e5e732fd1c3a45
abdygany_tabyldyev <tabyldyev34@gmail.com>
2020-05-23 19:06:56 +0600

    Exam 09

---
+++ exam9/src/main/resources/db/migration/V1005__add_comments.sql
@@ -0,0 +1,12 @@
+use `forum`;
+
+insert into `comments` (`text`, `user`, `theme`, `date`) values ('Sport1', 1, 1, NOW()), ('Sport2', 1, 1, NOW()),
+                                                                ('Sport3', 1, 1, NOW()),
+                                                                ('Sport3', 1, 1, NOW()),('Sport4545', 1, 1, NOW()),
+                                                                ('Sport4', 1, 1, NOW()),
+                                                                ('Sportdd', 1, 1, NOW()),
+                                                                ('Sport34', 1, 1, NOW()),
+                                                                ('Sport234523', 1, 1, NOW()),
+                                                                ('Sport234523', 1, 1, NOW()),
+                                                                ('Spor1111t', 1, 1, NOW()),
+                                                                ('Sport123123', 1, 1, NOW());
